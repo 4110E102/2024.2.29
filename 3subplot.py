996dc5614faed0174b56bc868e5df0470dbbf6ed
qiyiguo6133 <113968950+4110E102@users.noreply.github.com>
2024-03-22 16:14:20 +0800

    Add files via upload

---
+++ 3subplot.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+plt.subplot(1, 3, 1)
+plt.plot([1, 2, 3], [4, 5, 6])
+
+plt.subplot(1, 3, 2)
+plt.plot([7, 8, 9], [10, 11, 12])
+
+plt.subplot(1, 3, 3)
+plt.plot([13, 14, 15], [16, 17, 18])
+
+plt.show()
+
+
