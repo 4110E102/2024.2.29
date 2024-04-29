e40768b72b4468b8853242e8d792ee18d9ad1d88
qiyiguo6133 <113968950+4110E102@users.noreply.github.com>
2024-04-29 10:08:04 +0800

    Add files via upload

--- ex8301.php
+++ ex8301.php
@@ -1,20 +1,14 @@
 <?php
-
-    $numbers =array(16, 62, 53, 54, 55);
+    $numbers = array(1, 2, 3, 4, 5);
     $colors = [
         "red" => "紅色",
         "green" => "綠色",
-        "blue" =>"藍色"
+        "blue" => "藍色",
     ];
-    echo "第一個數字: " . $numbers[0] . "<br>";
-    echo "顏色'green': " . $colors['green'] . "<br><br>";
-
+    echo "第一個數字:" . $numbers[0] . "<br>";
+    echo "顏色 'green': " . $colors['green'] . "<br><br>"; 
     echo "所有數字:<br><br>";
-    foreach ($numbers as $number) {
+    foreach ($numbers as $number){
         echo $number . "<br>";
-    }
-    echo "<br>所有顏色:<br>";
-    foreach ($colors as $key => $color) {
-        echo $key . ":" .$color . "<br>";
     }
 ?>
